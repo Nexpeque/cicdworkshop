9e61313132c355bbc657ce3249bb9802ead37868
Daniel Nempeque N <32418528+DanielNempeque@users.noreply.github.com>
2021-04-29 12:28:34 -0500

    remove dev from title

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ export default class Header extends PureComponent {
           height="32"
           alt=""
         />
-        Emoji Search Dev!
+        Emoji Search :D!
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
           width="32"
