bc65dad128edbd1eabc7831d22ad7ffed52a871a
davesco2001 <71621788+davesco2001@users.noreply.github.com>
2021-05-07 15:44:36 -0500

    New Title

--- src/Header.js
+++ src/Header.js
@@ -11,7 +11,7 @@ export default class Header extends PureComponent {
           height="32"
           alt=""
         />
-        Emoji Search!
+        Error404!
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
           width="32"
