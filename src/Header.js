91853516c61e22a129951d1ad9ffda991783adc7
Daniel Nempeque <daniel.nempeque@endava.com>
2022-02-13 12:04:49 -0500

    Change header's emoji size

--- src/Header.js
+++ src/Header.js
@@ -7,15 +7,15 @@ export default class Header extends PureComponent {
       <header className="component-header">
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
-          width="32"
-          height="32"
+          width="50"
+          height="50"
           alt=""
         />
         Emoji Search!
         <img
           src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
-          width="32"
-          height="32"
+          width="50"
+          height="50"
           alt=""
         />
       </header>
